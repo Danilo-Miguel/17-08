5f92faaa8af10589b220eb137b898819d4f23eeb
Danilo de Souza Miguel <danilo.miguel@cromai.com>
2022-08-17 13:29:41 +0000

    Aula 3 antes do intervalo, criação de post mocados

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask("hello")
-nomeAula = "Aula python para Web"
+posts = [
+{
+    "title": " Meu primeiro post",
+    "body": "Esse post é o primeiro do blog",
+    "author": "Danilo de Souza Miguel",
+    "created": datetime(2022,8,17)
+},
+{
+    "title": " Meu segundo post",
+    "body": "Esse post é o segundo do blog",
+    "author": "Raiane Caroline",
+    "created": datetime(2022,8,17)
+},
+{
+    "title": " Meu terceiro post",
+    "body": "Esse post é o terceito do blog",
+    "author": "Jezimiel Marcondes",
+    "created": datetime(2022,8,17)
+},
+]
 
-@app.route("/usuario")
-def usuario():
-    usuario = [1, "Danilo de Souza Miguel", "Professor"]
-    alunos = ["Andre Guedes", "Lucas Santos", "Alicia Duarte", "Raiane Caroline"]
-    return render_template ("index.html", usuario=usuario, nome=nomeAula, alunos=alunos)
-
-@app.route("/contatos")
-def contato():
-    return render_template("index.html", usuario=usuario, nome=nomeAula)
+@app.route("/")
+def index():
+    return render_template("index.html", posts=posts)
